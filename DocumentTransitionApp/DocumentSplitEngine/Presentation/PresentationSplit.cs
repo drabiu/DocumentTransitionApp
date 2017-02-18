4b3ca10bde45953e03fd6e22d55ebb747dd6857d
drabiu <drabiu@7627e817-e702-434d-9e96-cce080ae4021>
2017-02-18 17:40:32 +0000

    update open xml to 2.6.1

--- DocumentTransitionApp/DocumentSplitEngine/Presentation/PresentationSplit.cs
+++ DocumentTransitionApp/DocumentSplitEngine/Presentation/PresentationSplit.cs
@@ -1,4 +1,5 @@
 ﻿using DocumentEditPartsEngine;
+using Drawing = DocumentFormat.OpenXml.Drawing;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentSplitEngine.Data_Structures;
 using SplitDescriptionObjects;
@@ -7,13 +8,13 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Xml.Serialization;
-using Presentproc = DocumentFormat.OpenXml.Presentation;
+using DocumentFormat.OpenXml.Presentation;
 
 namespace DocumentSplitEngine
 {
     public interface IPresentationMarkerMapper
     {
-        IList<OpenXMLDocumentPart<Presentproc.SlideId>> Run();
+        IList<OpenXMLDocumentPart<SlideId>> Run();
     }
 
     public class MarkerPresentationMapper : MarkerMapper, IPresentationMarkerMapper
@@ -39,9 +40,9 @@ namespace DocumentSplitEngine
         /// Finds parts of documents selected by the marker and returns as a list of persons each containing list of document elements
         /// </summary>
         /// <returns></returns>
-        public IList<OpenXMLDocumentPart<Presentproc.SlideId>> Run()
+        public IList<OpenXMLDocumentPart<SlideId>> Run()
 		{
-            IList<OpenXMLDocumentPart<Presentproc.SlideId>> documentElements = new List<OpenXMLDocumentPart<Presentproc.SlideId>>();
+            IList<OpenXMLDocumentPart<SlideId>> documentElements = new List<OpenXMLDocumentPart<SlideId>>();
             if (SplitPresentationObj != null)
             {
                 foreach (Person person in SplitPresentationObj.Person)
@@ -65,14 +66,14 @@ namespace DocumentSplitEngine
                 }
 
                 string email = string.Empty;
-                OpenXMLDocumentPart<Presentproc.SlideId> part = new OpenXMLDocumentPart<Presentproc.SlideId>();
+                OpenXMLDocumentPart<SlideId> part = new OpenXMLDocumentPart<SlideId>();
                 var slidePartsList = Presentation.Presentation.SlideIdList.ChildElements;
                 for (int index = 0; index < slidePartsList.Count; index++)
                 {
                     if (SubdividedParagraphs[index] != email)
                     {
-                        part = new OpenXMLDocumentPart<Presentproc.SlideId>();
-                        part.CompositeElements.Add(slidePartsList[index] as Presentproc.SlideId);
+                        part = new OpenXMLDocumentPart<SlideId>();
+                        part.CompositeElements.Add(slidePartsList[index] as SlideId);
                         email = SubdividedParagraphs[index];
                         if (string.IsNullOrEmpty(email))
                             part.PartOwner = "undefined";
@@ -82,7 +83,7 @@ namespace DocumentSplitEngine
                         documentElements.Add(part);
                     }
                     else
-                        part.CompositeElements.Add(slidePartsList[index] as Presentproc.SlideId);
+                        part.CompositeElements.Add(slidePartsList[index] as SlideId);
                 }
             }
 
@@ -90,7 +91,7 @@ namespace DocumentSplitEngine
         }
 	}
 
-	public class PresentationSplit : MergeXml<Presentproc.SlideId>, ISplit, ILocalSplit
+	public class PresentationSplit : MergeXml<SlideId>, ISplit, ILocalSplit
 	{
         public PresentationSplit(string docName)
         {
@@ -134,58 +135,25 @@ namespace DocumentSplitEngine
                     PresentationDocument.Open(mem, true))
                 {
                     PresentationPart presentationPart = preDoc.PresentationPart;
-                    using (PresentationDocument templateDocument = PresentationDocument.Open(mem, false))
+                    //using (PresentationDocument templateDocument = PresentationDocument.Open(mem, false))
+                    //{
+                    //    foreach (SlideId slideId in templateDocument.PresentationPart.Presentation.SlideIdList.ChildElements)
+                    //    {
+                    //        presentationPart.DeletePart(slideId.RelationshipId);
+                    //    }
+                    //}
+                    foreach (OpenXMLDocumentPart<SlideId> element in DocumentElements)
                     {
-                        foreach (Presentproc.SlideId slideId in templateDocument.PresentationPart.Presentation.SlideIdList.ChildElements)
-                        {
-                            presentationPart.DeletePart(slideId.RelationshipId);
-                        }
-                    }
-                    foreach (OpenXMLDocumentPart<Presentproc.SlideId> element in DocumentElements)
-                    {
-                        uint maxSlideId = 0;
                         //RemoveAllSlides(presentationPart);
 
                         //alternative RemoveAllSlides
-                        presentationPart.Presentation = new Presentproc.Presentation();
-                        presentationPart.Presentation.SlideIdList = new Presentproc.SlideIdList();         
+                        //presentationPart.Presentation = new Presentation();
+                        //presentationPart.Presentation.SlideIdList = new SlideIdList();         
 
-                        foreach (Presentproc.SlideId compo in element.CompositeElements)
+                        foreach (SlideId compo in element.CompositeElements)
                         {
-                            var slideIdList = presentationPart.Presentation.SlideIdList;
-                            //Find the highest slide ID in the current list.
-                            
-                            foreach (Presentproc.SlideId slideId in slideIdList.ChildElements)
-                            {
-                                if (slideId.Id.Value > maxSlideId)
-                                {
-                                    maxSlideId = slideId.Id;
-                                }
-                            }
-
-                            maxSlideId++;
-                            //Create the slide part and copy the data from the first part
-                            SlidePart newSlidePart = presentationPart.AddNewPart<SlidePart>();
-                            using (PresentationDocument templateDocument = PresentationDocument.Open(mem, false))
-                            {
-                                var templateSlide = (SlidePart)templateDocument.PresentationPart.GetPartById(compo.RelationshipId);
-                                newSlidePart.FeedData(templateSlide.GetStream());
-                                //Use the same slide layout as that of the template slide.
-                                if (templateSlide.SlideLayoutPart != null)
-                                {
-                                    newSlidePart.AddPart(templateSlide.SlideLayoutPart);
-                                }
-                            }
-
-                            newSlidePart.Slide.Save();
-
-                            //Insert the new slide into the slide list.
-                            Presentproc.SlideId newSlideId = slideIdList.AppendChild(new Presentproc.SlideId());
-                            //Presentproc.SlideId newSlideId = slideIdList.InsertAfter(new Presentproc.SlideId(), slideIdList.Last());
-
-                            //Set the slide id and relationship id
-                            newSlideId.Id = maxSlideId;
-                            newSlideId.RelationshipId = presentationPart.GetIdOfPart(newSlidePart);
+                            //InsertSlideFromTemplate(presentationPart, mem);
+                            InsertNewSlide(preDoc, 1, "aaaa");
                         }
 
                         presentationPart.Presentation.Save();
@@ -244,12 +212,180 @@ namespace DocumentSplitEngine
             return docSplit.PartsFromSplitXml(xmlFile, parts);
         }
 
+        #region Private methods
+
+        private void InsertSlideFromTemplate(PresentationPart presentationPart, MemoryStream mem)
+        {
+            uint maxSlideId = 0; 
+
+            var slideIdList = presentationPart.Presentation.SlideIdList;
+            //Find the highest slide ID in the current list.
+
+            foreach (SlideId slideId in slideIdList.ChildElements)
+            {
+                if (slideId.Id.Value > maxSlideId)
+                {
+                    maxSlideId = slideId.Id;
+                }
+            }
+
+            maxSlideId++;
+            //Create the slide part and copy the data from the first part
+            SlidePart newSlidePart = presentationPart.AddNewPart<SlidePart>();
+            Slide slide = new Slide(new CommonSlideData(new ShapeTree()));
+            //using (PresentationDocument templateDocument = PresentationDocument.Open(mem, false))
+            //{
+            //    var templateSlide = (SlidePart)templateDocument.PresentationPart.GetPartById(compo.RelationshipId);
+            //    newSlidePart.FeedData(templateSlide.GetStream());
+            //    //Use the same slide layout as that of the template slide.
+            //    if (templateSlide.SlideLayoutPart != null)
+            //    {
+            //        newSlidePart.AddPart(templateSlide.SlideLayoutPart);
+            //    }
+            //}
+
+            slide.Save(newSlidePart);
+
+            //Insert the new slide into the slide list.
+            SlideId newSlideId = slideIdList.AppendChild(new SlideId());
+            //SlideId newSlideId = slideIdList.InsertAfter(new SlideId(), slideIdList.Last());
+
+            //Set the slide id and relationship id
+            newSlideId.Id = maxSlideId;
+            newSlideId.RelationshipId = presentationPart.GetIdOfPart(newSlidePart);
+        }
+
+        private void InsertNewSlide(PresentationDocument presentationDocument, int position, string slideTitle)
+        {
+
+            if (presentationDocument == null)
+            {
+                throw new ArgumentNullException("presentationDocument");
+            }
+
+            if (slideTitle == null)
+            {
+                throw new ArgumentNullException("slideTitle");
+            }
+
+            PresentationPart presentationPart = presentationDocument.PresentationPart;
+
+            // Verify that the presentation is not empty.
+            if (presentationPart == null)
+            {
+                throw new InvalidOperationException("The presentation document is empty.");
+            }
+
+            // Declare and instantiate a new slide.
+            Slide slide = new Slide(new CommonSlideData(new ShapeTree()));
+            uint drawingObjectId = 1;
+
+            // Construct the slide content.            
+            // Specify the non-visual properties of the new slide.
+            NonVisualGroupShapeProperties nonVisualProperties = slide.CommonSlideData.ShapeTree.AppendChild(new NonVisualGroupShapeProperties());
+            nonVisualProperties.NonVisualDrawingProperties = new NonVisualDrawingProperties() { Id = 1, Name = "" };
+            nonVisualProperties.NonVisualGroupShapeDrawingProperties = new NonVisualGroupShapeDrawingProperties();
+            nonVisualProperties.ApplicationNonVisualDrawingProperties = new ApplicationNonVisualDrawingProperties();
+
+            // Specify the group shape properties of the new slide.
+            slide.CommonSlideData.ShapeTree.AppendChild(new GroupShapeProperties());
+
+            // Declare and instantiate the title shape of the new slide.
+            Shape titleShape = slide.CommonSlideData.ShapeTree.AppendChild(new Shape());
+
+            drawingObjectId++;
+
+            // Specify the required shape properties for the title shape. 
+            titleShape.NonVisualShapeProperties = new NonVisualShapeProperties
+                (new NonVisualDrawingProperties() { Id = drawingObjectId, Name = "Title" },
+                new NonVisualShapeDrawingProperties(new Drawing.ShapeLocks() { NoGrouping = true }),
+                new ApplicationNonVisualDrawingProperties(new PlaceholderShape() { Type = PlaceholderValues.Title }));
+            titleShape.ShapeProperties = new ShapeProperties();
+
+            // Specify the text of the title shape.
+            titleShape.TextBody = new TextBody(new Drawing.BodyProperties(),
+                    new Drawing.ListStyle(),
+                    new Drawing.Paragraph(new Drawing.Run(new Drawing.Text() { Text = slideTitle })));
+
+            // Declare and instantiate the body shape of the new slide.
+            Shape bodyShape = slide.CommonSlideData.ShapeTree.AppendChild(new Shape());
+            drawingObjectId++;
+
+            // Specify the required shape properties for the body shape.
+            bodyShape.NonVisualShapeProperties = new NonVisualShapeProperties(new NonVisualDrawingProperties() { Id = drawingObjectId, Name = "Content Placeholder" },
+                    new NonVisualShapeDrawingProperties(new Drawing.ShapeLocks() { NoGrouping = true }),
+                    new ApplicationNonVisualDrawingProperties(new PlaceholderShape() { Index = 1 }));
+            bodyShape.ShapeProperties = new ShapeProperties();
+
+            // Specify the text of the body shape.
+            bodyShape.TextBody = new TextBody(new Drawing.BodyProperties(),
+                    new Drawing.ListStyle(),
+                    new Drawing.Paragraph());
+
+            // Create the slide part for the new slide.
+            SlidePart slidePart = presentationPart.AddNewPart<SlidePart>();
+
+            // Save the new slide part.
+            slide.Save(slidePart);
+
+            // Modify the slide ID list in the presentation part.
+            // The slide ID list should not be null.
+            SlideIdList slideIdList = presentationPart.Presentation.SlideIdList;
+
+            // Find the highest slide ID in the current list.
+            uint maxSlideId = 1;
+            SlideId prevSlideId = null;
+
+            foreach (SlideId slideId in slideIdList.ChildElements)
+            {
+                if (slideId.Id > maxSlideId)
+                {
+                    maxSlideId = slideId.Id;
+                }
+
+                position--;
+                if (position == 0)
+                {
+                    prevSlideId = slideId;
+                }
+
+            }
+
+            maxSlideId++;
+
+            // Get the ID of the previous slide.
+            SlidePart lastSlidePart;
+
+            if (prevSlideId != null)
+            {
+                lastSlidePart = (SlidePart)presentationPart.GetPartById(prevSlideId.RelationshipId);
+            }
+            else
+            {
+                lastSlidePart = (SlidePart)presentationPart.GetPartById(((SlideId)(slideIdList.ChildElements[0])).RelationshipId);
+            }
+
+            // Use the same slide layout as that of the previous slide.
+            if (null != lastSlidePart.SlideLayoutPart)
+            {
+                slidePart.AddPart(lastSlidePart.SlideLayoutPart);
+            }
+
+            // Insert the new slide into the slide list after the previous slide.
+            SlideId newSlideId = slideIdList.InsertAfter(new SlideId(), prevSlideId);
+            newSlideId.Id = maxSlideId;
+            newSlideId.RelationshipId = presentationPart.GetIdOfPart(slidePart);
+
+            // Save the modified presentation.
+            presentationPart.Presentation.Save();
+        }
+
         private void RemoveAllSlides(PresentationPart presentationPart)
         {
-            Presentproc.Presentation presentation = presentationPart.Presentation;
-            Presentproc. SlideIdList slideIdList = presentation.SlideIdList;
+            Presentation presentation = presentationPart.Presentation;
+             SlideIdList slideIdList = presentation.SlideIdList;
 
-            foreach (Presentproc.SlideId slideId in slideIdList.ChildElements.ToList())
+            foreach (SlideId slideId in slideIdList.ChildElements.ToList())
             {
                 slideIdList.RemoveChild(slideId);
                 string slideRelId = slideId.RelationshipId;
@@ -257,13 +393,13 @@ namespace DocumentSplitEngine
                 if (presentation.CustomShowList != null)
                 {
                     // Iterate through the list of custom shows.
-                    foreach (var customShow in presentation.CustomShowList.Elements<Presentproc.CustomShow>())
+                    foreach (var customShow in presentation.CustomShowList.Elements<CustomShow>())
                     {
                         if (customShow.SlideList != null)
                         {
                             // Declare a link list of slide list entries.
-                            LinkedList<Presentproc.SlideListEntry> slideListEntries = new LinkedList<Presentproc.SlideListEntry>();
-                            foreach (Presentproc.SlideListEntry slideListEntry in customShow.SlideList.Elements())
+                            LinkedList<SlideListEntry> slideListEntries = new LinkedList<SlideListEntry>();
+                            foreach (SlideListEntry slideListEntry in customShow.SlideList.Elements())
                             {
                                 // Find the slide reference to remove from the custom show.
                                 if (slideListEntry.Id != null && slideListEntry.Id == slideRelId)
@@ -273,7 +409,7 @@ namespace DocumentSplitEngine
                             }
 
                             // Remove all references to the slide from the custom show.
-                            foreach (Presentproc.SlideListEntry slideListEntry in slideListEntries)
+                            foreach (SlideListEntry slideListEntry in slideListEntries)
                             {
                                 customShow.SlideList.RemoveChild(slideListEntry);
                             }
@@ -282,6 +418,7 @@ namespace DocumentSplitEngine
                 }
             }
         }
-            
+
+        #endregion
     }
 }
