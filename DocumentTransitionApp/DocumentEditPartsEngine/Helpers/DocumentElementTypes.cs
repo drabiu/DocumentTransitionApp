077089bae61d29560407ca6d112dffa1ddefd6e2
drabiu <drabiozo@o2.pl>
2017-03-15 00:58:06 +0100

    Add files via upload

---
+++ DocumentTransitionApp/DocumentEditPartsEngine/Helpers/DocumentElementTypes.cs
@@ -0,0 +1,16 @@
+﻿namespace DocumentEditPartsEngine.Helpers
+{
+    public enum ElementType
+    {
+        Paragraph,
+        BulletList,
+        NumberedList,
+        Table,
+        Picture,
+        Sheet,
+        Column,
+        Row,
+        Cell,
+        Slide
+    }
+}
